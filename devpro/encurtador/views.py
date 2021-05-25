24474753b0e988f0ea1047e65359a2b8d38b834e
dantls <dan_tls@hotmail.com>
2021-05-24 23:45:06 -0400

    add admin

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -2,10 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlLog
 
 def redirecionar(requisicao, slug):
     #return HttpResponse(f'Olá Django: {slug}')
     url_redirect = UrlRedirect.objects.get(slug=slug)
+    UrlLog.objects.create(
+        origem = requisicao.META.get('TTP_REFERER'),
+        user_agent = requisicao.META.get('HTTP_USER_AGENT'),
+        host = requisicao.META.get('HTTP_HOST'),
+        ip = requisicao.META.get('HTTP_ADDR'),
+        url_redirect = url_redirect
+    )
     return redirect(url_redirect.destino)
 
 def relatorios(requisicao, slug):
